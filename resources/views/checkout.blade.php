2cabd052f9253fea01ef4bc3f481db23c7d4dc51
AkulKalu <70473314+AkulKalu@users.noreply.github.com>
2021-02-09 19:28:07 +0100

    WIP...

---
+++ resources/views/checkout.blade.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Iceland&display=swap" rel="stylesheet">
+    <link rel="stylesheet"  href="{{ asset('css/checkout.css') }}">
+    <script src="{{ asset('js/checkout.js') }}"  defer ></script>
+    <script src="https://www.paypalobjects.com/api/checkout.js"></script>
+    <script src="https://js.stripe.com/v3/"></script>
+    <title>Product</title>
+</head>
+<body>
+    @if(!$product->stock) 
+        <div class="Barrier">
+            Out of stock!
+        </div>
+    @elseif(!$product->active)
+        <div class="Barrier">
+            Currently not in offer.
+        </div>
+    @endif
+    <div id="imgBg" class="Img" >
+        <img  src={{ $product->image_url}} alt="ProductImage">
+    </div>
+    <main>
+        <div class="Info">
+            <div class="InfoLarge">{{ $product->manufacturer}}</div>
+            <div class="InfoSmall">
+                <span >{{ $product->category}}</span>
+                <span class="border-left">{{ $product->model}} </span>
+            </div>
+            
+            <div class="Price"> {{ $product->currency}}{{ $product->price}} </div>
+        </div>
+        <div class="Buttons">
+            <div id="aboutBtn" class="Btn">
+                <span>Info</span>
+            </div> 
+          
+            <div id="buyBtn" class="Btn">
+                <span>Buy</span>
+            </div>
+        </div>
+    </main>
+    @isset($purchaseCompleted )        
+        <div class="Success" id="succesWindow">
+            <div class="Message">
+                <div>
+                    <p>Your {{$purchaseCompleted}} transaction has been successfull.</p>
+                    <p>Details have been sent to your email addres, if you have any questions contact us at:</p>
+                    <a href="mailto: {{ $product->store->email }}" >{{ $product->store->email }}</a>
+                    @isset($product->store->phone )
+                        <p>or by phone: {{ $product->store->phone }}</p>
+                    @endisset
+                    <div id="closeSuccessBtn" class="Btn">Thanks!</div>
+                </div>
+                
+            </div>
+        </div>
+    @endisset
+    <!-- product info -->
+    <div id="description"  class="Window  Description" >
+        {{ $product->description}}
+    </div>
+    <!--  -->
+
+    <!-- buy window -->
+    <div id="buyMenu"  class="Window BuyMenu" >
+        <div class="Quantaty">
+            <h2>Quantaty</h2>
+            <div id='counter' class="Counter">
+                <div id="qL" class="Left"></div>
+                <div id="qTotal" class="Count" >1</div>
+                <div id="qR" class="Right"></div>
+            </div>
+        </div>
+        <h2>Delivery</h2>
+        <div id="delivery" class="Delivery">
+            <span id="yes">Yes</span>
+            <span style="opacity: 0.6;" id="no">No</span>
+        </div>
+        <h2>Payment</h2>
+        <div class="Payment">
+         <input id="stripe" class="Method" type="button" value="Stripe">
+         <div class="PayPal">Paypal 
+            <div class="PayPalBtn"  id="paypalBtn"><div>
+         </div>
+            
+        </div>
+    </div>
+    <!--  -->
+    <script>
+        // Create an instance of the Stripe object with your publishable API key
+        let publicKey = "{{$public_key}}";
+        let productId = "{{$product->id}}";
+        let isDemo = "{{$product->store->user->id}}";
+        let stock = "{{$product->stock}}";
+        let theme = @json($theme);
+    </script>
+</body>
+ 
+</body>
+</html>
