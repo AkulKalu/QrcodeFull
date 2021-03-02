56de072178125b8d9151d1a60745f08e553318b4
AkulKalu <70473314+AkulKalu@users.noreply.github.com>
2021-03-02 19:00:30 +0100

    restyling...

--- resources/js/components/MainPage/Card.js
+++ resources/js/components/MainPage/Card.js
@@ -18,7 +18,7 @@ export default function Card(props) {
                 <h2 className="Underlinie">Step 1</h2>
                 <p>Simply, create a store and add your contact and api keys.</p>
             </div>
-            images = ['https://i.ibb.co/5s4xr3S/qrcode-3.jpg'
+            images = ['https://res.cloudinary.com/df6ytp5bl/image/upload/v1614707471/portfolio/qrcodes-2_wysj1r.jpg'
         ]
             break;
         case  2:
@@ -26,20 +26,20 @@ export default function Card(props) {
             <h2 className="Underlinie">Step 2</h2>
             <p>Add items, and make your own color theme for the checkout page. </p>
         </div>
-            images = ['https://i.ibb.co/KWSTpyg/qrcode-4.jpg',]
+            images = ['https://res.cloudinary.com/df6ytp5bl/image/upload/v1614707472/portfolio/qrcode-4_htrumo.jpg',]
             break;
         case  3:
             textContent = <div>
             <p>Use our modern control panel to keep track of your items, generate or download codes, and make changes.</p>
             </div>
-            images = ['https://i.ibb.co/SQfZvrs/qrcode-1.jpg',]
+            images = ['https://res.cloudinary.com/df6ytp5bl/image/upload/v1614707472/portfolio/qrcodes-3_jfv4mv.jpg',]
             break;
         case  4:
             textContent = <div>
             <p>You can also keep track of any transactions and related shippments that need to be sent</p>
             <p>Simply click the envelope button when merch is on the way and the customer will be notified</p>
             </div>
-            images = ['https://i.ibb.co/0Dnxw1M/qrcode-5.jpg', 'https://i.ibb.co/mX4svRs/qrcode-2.jpg']
+            images = ['https://res.cloudinary.com/df6ytp5bl/image/upload/v1614707474/portfolio/qrcode-5_blyzhk.jpg', 'https://res.cloudinary.com/df6ytp5bl/image/upload/v1614707473/portfolio/qrcode-6_pghj7v.jpg']
             break;
         
         default:
